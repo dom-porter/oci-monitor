9cd6acc095c7a01ee5960f49e797141d85164a11
dporter <dominic.porter@dbpsolutions.com>
2023-10-12 19:52:30 +0700

    initial commit

--- iaas/enums.py
+++ iaas/enums.py
@@ -2,5 +2,7 @@ from enum import Enum, auto
 
 
 class Providers(Enum):
+    """ A list of supported IaaS providers """
+
     ORACLE = auto()
     AWS = auto()
--- iaas/vm.py
+++ iaas/vm.py
@@ -5,6 +5,8 @@ from oci.core.models import instance
 
 @dataclass
 class VirtualMachine:
+    """ Basic information about the VM """
+
     display_name: str
     vm_id: str
     state: str
@@ -15,6 +17,7 @@ def oracle_vm_factory(vm: instance) -> VirtualMachine:
     Creates an instance of VirtualMachine class.
     Factory does not maintain any of the instances it creates.
     """
+
     return VirtualMachine(
         display_name=vm.display_name,
         vm_id=vm.id,
